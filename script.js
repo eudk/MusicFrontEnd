e1ed3f5d6ea33c3d457125f12a65c8719ba2adfe
Jonathan-Ramus <ramus7000@gmail.com>
2023-11-16 13:59:40 +0100

    Added filters

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const baseUri = "https://musicrest20231109141315.azurewebsites.net/records"
+//const baseUri = "http://localhost:5242/"
 
 Vue.createApp({
     data() {
@@ -7,14 +8,22 @@ Vue.createApp({
 
             records: [],
             id : '',
+            f_title : '',
+            f_artist : '',
+            f_duration : 0,
+            f_pubyear : null,
             error: null
         }
     },
     methods: {
         async getAll() {
+            params = new URLSearchParams([['title', this.f_title], ['artist', this.f_artist], ['duration', this.f_duration]]);
+            if (this.f_pubyear != null) {
+                params.append('publicationYear', this.f_pubyear);
+            }
             console.log(baseUri)
             try {
-                const response = await axios.get(baseUri)
+                const response = await axios.get(baseUri, { params });
                 this.records = await response.data
                 this.error = null
             } catch (ex) {
